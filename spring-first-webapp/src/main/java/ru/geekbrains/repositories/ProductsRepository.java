27f62b5987c391316d13a91c10e6cc262079508f
sujane <sujane_mf@mail.ru>
2022-05-29 21:25:21 +0300

    Домашнее задание к уроку 3: 1) Главная страница: к списку продуктов, добавление продуктов 2) Страница со списком продуктов 3) Страница с информацией по продукту по ID 4) Форма добавления продукта

---
+++ spring-first-webapp/src/main/java/ru/geekbrains/repositories/ProductsRepository.java
@@ -0,0 +1,40 @@
+package ru.geekbrains.repositories;
+
+import org.springframework.stereotype.Controller;
+import ru.geekbrains.model.Product;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Controller
+public class ProductsRepository {
+    private List<Product> products;
+
+    @PostConstruct
+    public void init(){
+        this.products = new ArrayList<>();
+        products.add(new Product(1L,"Milk",5));
+        products.add(new Product(2L,"Book",10));
+        products.add(new Product(3L,"Box",1));
+        products.add(new Product(4L,"Plate",7));
+        products.add(new Product(5L,"Cup",3));
+
+    }
+    // метод для просмотра всего списка товаров
+    public List<Product> showAll(){
+        return Collections.unmodifiableList(products);
+    }
+
+    // метод для поиска товаров по ID
+    public Product getProductByID(Long id){return products.stream().filter(s -> s.getId().equals(id)).findFirst().get();}
+
+    // метод для создания товара
+    public void createProduct(Product product){
+        products.add(product);
+    }
+
+
+
+}
